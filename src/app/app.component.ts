523bb094109cb65c7600d0751f03210c5df9683b
Ravindra Devrani <ravindra.devrani003@gmail.com>
2022-10-06 16:49:12 +0530

    abc

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { user } from './models/user';
+import { UserServices } from './services/user-services';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +8,34 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'yt-angular-mock-server';
+  
+  
+  constructor(private userService:UserServices) {
+    this.getUsers();
+  }
+  users!:user[];
+  user:user={id:0,name:'',email:''};
+  
+  addUser(){
+    this.userService.addUser(this.user).subscribe({
+      next: (res)=>{
+          this.users.push(this.user);
+      },
+      error: (err)=>{
+        console.log(err);
+      }
+    })
+  }
+
+  private getUsers(){
+    this.userService.getUsers().subscribe({
+       next:(res)=>{
+        this.users=res;
+       },
+       error:(err)=>console.log(err)
+    })
+  }
+
 }
+
+
